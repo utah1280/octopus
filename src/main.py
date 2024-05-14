bda6787e125aaefa0f298791d4cc4968bb78e96b
taboo9090 <ravshan.yaml@gmail.com>
2024-05-14 23:17:51 +0500

    octopus

--- src/main.py
+++ src/main.py
@@ -1,11 +1,47 @@
-import sys
+import os
 import gi
+import sys
+import logging
 
 gi.require_version('Gtk', '4.0')
 gi.require_version('Adw', '1')
 
 from gi.repository import Gtk, Gio, Adw
 from .window import OctopusWindow
+
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+# Helper functions
+FILE_TYPE_MAP = {
+    'text': ['.txt', '.text'],
+    'picture': ['.jpg', '.jpeg', '.png', '.gif', '.svg'],
+    'audio': ['.mp3', '.wav', '.flac'],
+    'video': ['.mp4', '.mov', '.avi'],
+    'archive': ['.zip', '.tar', '.gz', '.rar'],
+}
+
+def get_file_type(filename):
+    """Returns the type of the file based on its extension."""
+    extension = os.path.splitext(filename)[1].lower()
+    if not extension:
+        return 'directory'
+    for file_type, extensions in FILE_TYPE_MAP.items():
+        if extension in extensions:
+            return file_type
+    return 'unknown'
+
+def list_directory_files(directory, show_hidden=False):
+    """Returns a list of files in the given directory with their types."""
+    files = os.listdir(directory)
+    if not show_hidden:
+        files = [file for file in files if not file.startswith('.')]
+    return [{'name': file, 'type': get_file_type(file)} for file in files]
+
+
 
 
 class OctopusApplication(Adw.Application):
@@ -15,8 +51,89 @@ class OctopusApplication(Adw.Application):
         super().__init__(application_id='com.octopus.octopus', flags=Gio.ApplicationFlags.DEFAULT_FLAGS)
 
         self.window = None
+        self.home_path = os.path.expanduser("~")
+        self.current_path = self.home_path
+
         self.create_actions()
 
+
+    # Callbacks
+    def on_about_action(self, widget, _):
+        """Callback for the app.about action."""
+        about = Adw.AboutWindow(transient_for=self.window,
+                                application_name='octopus',
+                                application_icon='com.octopus.octopus',
+                                developer_name='System Programming Project',
+                                version='0.1.0',
+                                developers=['Ravshan Zaripov', 'Bobir Ibragimov', 'Akbar Aminov'],
+                                copyright='© 2024 Ravshan Zaripov')
+        about.present()
+
+    def on_home(self, widget, _):
+        """Callback for the app.home action."""
+        self.update_file_list()
+
+    def on_documents(self, widget, _):
+        """Callback for the app.documents action"""
+        self.update_file_list(self.window.home_path + "/Documents")
+
+    def on_downloads(self, widget, _):
+        """Callback for the app.downloads action"""
+        self.update_file_list(self.window.home_path + "/Downloads")
+
+    def on_music(self, widget, _):
+        """Callback for the app.music action"""
+        self.update_file_list(self.window.home_path + "/Music")
+
+    def on_pictures(self, widget, _):
+        """Callback for the app.pictures action"""
+        self.update_file_list(self.window.home_path + "/Pictures")
+
+    def on_videos(self, widget, _):
+        """Callback for the app.videos action"""
+        self.update_file_list(self.window.home_path + "/Videos")
+
+    def on_trash(self, widget, _):
+        """Callback for the app.trash action."""
+        self.update_file_list(".local/share/Trash/files")
+
+    def on_search(self, widget, _):
+        """Callback for the app.search action."""
+        pass
+
+
+    #
+    def update_file_list(self, directory=None):
+        """Updates the file list in the UI for the given directory."""
+        directory = directory or self.home_path
+        files = list_directory_files(directory)
+
+        if not files:
+            status_page = Adw.StatusPage(title="Folder is empty", icon_name="folder-symbolic")
+            if self.window.content.get_content() != status_page:
+                self.window.content.set_content(status_page)
+            logger.info(f"No files found in directory: {directory}")
+            return
+
+        list_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, margin_top=5, margin_bottom=5, margin_start=10, margin_end=10, spacing=3)
+
+        self.window.list_view_content.set_child(list_box)
+        self.window.content.set_content()
+        self.window.content.set_content(self.window.list_view)
+
+        for file in files:
+            button = Gtk.Button(
+                child=Adw.ButtonContent(label=file['name'], halign=Gtk.Align.START, margin_start=5, icon_name=file['type']),
+                has_frame=False
+            )
+            button.connect("clicked", lambda btn, fname=file['name']: self.update_file_list(os.path.join(directory, fname)))
+            list_box.append(button)
+
+        self.current_path = directory
+        logger.info(f"Updated file list for directory: {directory}")
+
+
+    #
     def create_actions(self):
         """Create application actions."""
         actions = [
@@ -47,49 +164,10 @@ class OctopusApplication(Adw.Application):
         if not self.window:
             self.window = OctopusWindow(application=self)
         self.window.present()
+        self.update_file_list()
 
-    def on_about_action(self, widget, _):
-        """Callback for the app.about action."""
-        about = Adw.AboutWindow(transient_for=self.window,
-                                application_name='octopus',
-                                application_icon='com.octopus.octopus',
-                                developer_name='System Programming Project',
-                                version='0.1.0',
-                                developers=['Ravshan Zaripov', 'Bobir Ibragimov', 'Akbar Aminov'],
-                                copyright='© 2024 Ravshan Zaripov')
-        about.present()
 
-    def on_search(self, widget, _):
-        """Callback for the app.search action."""
-        pass
 
-    def on_home(self, widget, _):
-        """Callback for the app.home action."""
-        self.window.update_file_list(self.window.home_path)
-
-    def on_documents(self, widget, _):
-        """Callback for the app.documents action"""
-        self.window.update_file_list(self.window.home_path + "/Documents")
-
-    def on_downloads(self, widget, _):
-        """Callback for the app.downloads action"""
-        self.window.update_file_list(self.window.home_path + "/Downloads")
-
-    def on_music(self, widget, _):
-        """Callback for the app.music action"""
-        self.window.update_file_list(self.window.home_path + "/Music")
-
-    def on_pictures(self, widget, _):
-        """Callback for the app.pictures action"""
-        self.window.update_file_list(self.window.home_path + "/Pictures")
-
-    def on_videos(self, widget, _):
-        """Callback for the app.videos action"""
-        self.window.update_file_list(self.window.home_path + "/Videos")
-
-    def on_trash(self, widget, _):
-        """Callback for the app.trash action."""
-        self.window.update_file_list(".local/share/Trash/files")
 
 def main(version):
     """The application's entry point."""
--- src/window.py
+++ src/window.py
@@ -1,47 +1,12 @@
 import os
-import logging
 from gi.repository import Adw, Gtk
-
-# Configure logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-FILE_TYPE_MAP = {
-    'text': ['.txt', '.text'],
-    'picture': ['.jpg', '.jpeg', '.png', '.gif', '.svg'],
-    'audio': ['.mp3', '.wav', '.flac'],
-    'video': ['.mp4', '.mov', '.avi'],
-    'archive': ['.zip', '.tar', '.gz', '.rar'],
-}
-
-def get_home_directory():
-    """Returns the home directory of the current user."""
-    return os.path.expanduser("~")
-
-def get_file_type(filename):
-    """Returns the type of the file based on its extension."""
-    extension = os.path.splitext(filename)[1].lower()
-    if not extension:
-        return 'directory'
-    for file_type, extensions in FILE_TYPE_MAP.items():
-        if extension in extensions:
-            return file_type
-    return 'unknown'
-
-def list_directory_files(directory, show_hidden=False):
-    """Returns a list of files in the given directory with their types."""
-    files = os.listdir(directory)
-    if not show_hidden:
-        files = [file for file in files if not file.startswith('.')]
-    return [{'name': file, 'type': get_file_type(file)} for file in files]
 
 
 @Gtk.Template(resource_path='/com/octopus/octopus/window.ui')
 class OctopusWindow(Adw.ApplicationWindow):
     __gtype_name__ = 'OctopusWindow'
 
-    home_path = get_home_directory()
-    current_path = home_path
+    home_path = os.path.expanduser("~")
 
     button_names = [
         "search_button",
@@ -79,34 +44,3 @@ class OctopusWindow(Adw.ApplicationWindow):
 
         for button, path in tooltip_paths.items():
             button.set_tooltip_text(f"{self.home_path}{path}")
-
-        self.update_file_list(self.home_path)
-
-    def update_file_list(self, directory=None):
-        """Updates the file list in the UI for the given directory."""
-        directory = directory or self.home_path
-        files = list_directory_files(directory)
-
-        if not files:
-            status_page = Adw.StatusPage(title="Folder is empty", icon_name="folder-symbolic")
-            if self.content.get_content() != status_page:
-                self.content.set_content(status_page)
-            logger.info(f"No files found in directory: {directory}")
-            return
-
-        list_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, margin_top=5, margin_bottom=5, margin_start=10, margin_end=10, spacing=3)
-
-        self.list_view_content.set_child(list_box)
-        self.content.set_content()
-        self.content.set_content(self.list_view)
-
-        for file in files:
-            button = Gtk.Button(
-                child=Adw.ButtonContent(label=file['name'], halign=Gtk.Align.START, margin_start=5, icon_name=file['type']),
-                has_frame=False
-            )
-            button.connect("clicked", lambda btn, fname=file['name']: self.update_file_list(os.path.join(directory, fname)))
-            list_box.append(button)
-
-        self.current_path = directory
-        logger.info(f"Updated file list for directory: {directory}")
