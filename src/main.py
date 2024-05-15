0be027195e777e18d78ee65832d383a76945616f
taboo9090 <ravshan.yaml@gmail.com>
2024-05-15 05:54:39 +0500

    octopus

--- src/main.py
+++ src/main.py
@@ -8,14 +8,12 @@ import subprocess
 gi.require_version('Gtk', '4.0')
 gi.require_version('Adw', '1')
 
-from gi.repository import Gtk, Gio, Adw, GLib
+from gi.repository import Gtk, Gio, Adw
 from .window import OctopusWindow
-
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
 
 # Helper functions
 FILE_TYPE_MAP = {
@@ -50,30 +48,24 @@ def open_file(filepath):
     except subprocess.CalledProcessError as e:
         logger.error(f"Error opening file {filepath}: {e}")
 
-
+# Application class
 class OctopusApplication(Adw.Application):
     """The main application singleton class."""
 
     def __init__(self):
         super().__init__(application_id='com.octopus.octopus', flags=Gio.ApplicationFlags.DEFAULT_FLAGS)
-
         self.window = None
         self.home_path = os.path.expanduser("~")
         self.current_path = self.home_path
         self.search_active = False
-
         self.last_click_time = 0
         self.selected_button = None
-
         self.history = []
         self.history_index = -1
-
         self.create_actions()
-
 
     # Callbacks
     def on_about_action(self, widget, _):
-        """Callback for the app.about action."""
         about = Adw.AboutWindow(transient_for=self.window,
                                 application_name='octopus',
                                 application_icon='com.octopus.octopus',
@@ -84,73 +76,34 @@ class OctopusApplication(Adw.Application):
         about.present()
 
     def on_search(self, widget, _):
+        self.search_active = not self.search_active
         if self.search_active:
-            self.window.search_button.set_icon_name("system-search-symbolic")
-            self.window.search_button.set_tooltip_text("Search Everywhere")
-
-            self.window.content_topbar.set_title_widget()
-            self.window.content_topbar.set_title_widget(self.window.content_topbar_title)
-
-            self.window.content_topbar.pack_start(self.window.content_topbar_start)
-
-            self.update_file_list(self.current_path)
-
-            self.search_active = False
-        else:
             self.window.search_button.set_icon_name("folder-open-symbolic")
             self.window.search_button.set_tooltip_text("Go to Files")
-
-            self.window.content_topbar.set_title_widget()
             self.window.content_topbar.set_title_widget(self.window.search_widget)
-
             self.window.content_topbar.remove(self.window.content_topbar_start)
-
             self.window.content.set_content(Adw.StatusPage(icon_name="system-search-symbolic", title="Search Everywhere", description="Find files and folders in all search locations."))
+        else:
+            self.window.search_button.set_icon_name("system-search-symbolic")
+            self.window.search_button.set_tooltip_text("Search Everywhere")
+            self.window.content_topbar.set_title_widget(self.window.content_topbar_title)
+            self.window.content_topbar.pack_start(self.window.content_topbar_start)
+            self.update_file_list(self.current_path)
 
-            self.search_active = True
-
-    def on_home(self, widget, _):
-        """Callback for the app.home action."""
-        self.update_file_list()
-
-    def on_documents(self, widget, _):
-        """Callback for the app.documents action"""
-        self.update_file_list(self.window.home_path + "/Documents")
-
-    def on_downloads(self, widget, _):
-        """Callback for the app.downloads action"""
-        self.update_file_list(self.window.home_path + "/Downloads")
-
-    def on_music(self, widget, _):
-        """Callback for the app.music action"""
-        self.update_file_list(self.window.home_path + "/Music")
-
-    def on_pictures(self, widget, _):
-        """Callback for the app.pictures action"""
-        self.update_file_list(self.window.home_path + "/Pictures")
-
-    def on_videos(self, widget, _):
-        """Callback for the app.videos action"""
-        self.update_file_list(self.window.home_path + "/Videos")
-
-    def on_trash(self, widget, _):
-        """Callback for the app.trash action."""
-        self.update_file_list(os.path.expanduser("~/.local/share/Trash/files"))
+    def on_navigate(self, widget, target_path):
+        """Callback for navigation actions."""
+        self.update_file_list(target_path)
 
     def on_back(self, widget, _):
-        """Callback for the app.back action."""
         if self.history_index > 0:
             self.history_index -= 1
             self.update_file_list(self.history[self.history_index], add_to_history=False)
 
     def on_forward(self, widget, _):
-        """Callback for the app.forward action."""
         if self.history_index < len(self.history) - 1:
             self.history_index += 1
             self.update_file_list(self.history[self.history_index], add_to_history=False)
 
-
-    #
     def on_button_clicked(self, button, filename):
         current_time = time.time()
         full_path = os.path.join(self.current_path, filename)
@@ -160,20 +113,18 @@ class OctopusApplication(Adw.Application):
             else:
                 open_file(full_path)
             self.selected_button = None
-            self.last_click_time = 0
         else:
             if self.selected_button:
                 self.selected_button.get_style_context().remove_class("selected")
             self.selected_button = button
             self.last_click_time = current_time
-
             button.get_style_context().add_class("selected")
 
+    # Update UI
     def update_file_list(self, directory=None, add_to_history=True):
         """Updates the file list in the UI for the given directory."""
         directory = directory or self.home_path
         files = list_directory_files(directory)
-
         self.window.path_button.set_label(directory)
 
         if add_to_history:
@@ -184,14 +135,12 @@ class OctopusApplication(Adw.Application):
 
         if not files:
             status_page = Adw.StatusPage(title="Folder is empty", icon_name="folder-symbolic")
-            if self.window.content.get_content() != status_page:
-                self.window.content.set_content(status_page)
+            self.window.content.set_content(status_page)
             logger.info(f"No files found in directory: {directory}")
             self.current_path = directory
             return
 
         list_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, margin_top=5, margin_bottom=5, margin_start=10, margin_end=10, spacing=3)
-
         self.window.list_view_content.set_child(list_box)
         self.window.content.set_content()
         self.window.content.set_content(self.window.list_view)
@@ -207,23 +156,22 @@ class OctopusApplication(Adw.Application):
         self.current_path = directory
         logger.info(f"Updated file list for directory: {directory}")
 
-
-    #
+    # Create actions
     def create_actions(self):
         """Create application actions."""
         actions = [
             ('quit', lambda *_: self.quit(), ['<primary>q']),
             ('about', self.on_about_action, ['<primary>a']),
             ('search', self.on_search, ['<primary>s']),
-            ('home', self.on_home, None),
-            ('documents', self.on_documents, None),
-            ('downloads', self.on_downloads, None),
-            ('music', self.on_music, None),
-            ('pictures', self.on_pictures, None),
-            ('videos', self.on_videos, None),
-            ('trash', self.on_trash, None),
+            ('home', lambda widget, _: self.on_navigate(widget, self.home_path), None),
+            ('documents', lambda widget, _: self.on_navigate(widget, os.path.join(self.home_path, "Documents")), None),
+            ('downloads', lambda widget, _: self.on_navigate(widget, os.path.join(self.home_path, "Downloads")), None),
+            ('music', lambda widget, _: self.on_navigate(widget, os.path.join(self.home_path, "Music")), None),
+            ('pictures', lambda widget, _: self.on_navigate(widget, os.path.join(self.home_path, "Pictures")), None),
+            ('videos', lambda widget, _: self.on_navigate(widget, os.path.join(self.home_path, "Videos")), None),
+            ('trash', lambda widget, _: self.on_navigate(widget, os.path.expanduser("~/.local/share/Trash/files")), None),
             ('back', self.on_back, ['<primary>Left']),
-            ('forward', self.on_forward, ['<primary>Right'])
+            ('forward', self.on_forward, ['<primary>Right']),
         ]
         for name, callback, shortcuts in actions:
             self.create_action(name, callback, shortcuts)
@@ -243,10 +191,8 @@ class OctopusApplication(Adw.Application):
         self.window.present()
         self.update_file_list()
 
-
-
-
 def main(version):
     """The application's entry point."""
     app = OctopusApplication()
     return app.run(sys.argv)
+
